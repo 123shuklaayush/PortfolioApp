3c896a42da7a8be780f14bf927749d38a09eb697
Ayush Shukla <123shuklaayush@gmail.com>
2024-01-23 00:10:20 +0530

    modified the menu features

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -11,40 +11,45 @@ import { Link } from "react-router-dom";
 import myContext from "../context/AppContext";
 
 function SideBar() {
-  const {activeSideBar} = useContext(myContext)
-  const [open, setOpen] =useState(true);
+  const { activeSideBar, setActiveSideBar } = useContext(myContext);
+  const [open, setOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setOpen(!open);
+    if (activeSideBar) {
+      setActiveSideBar(false);
+    }
+  };
+
   return ( 
     <aside className={`side-bar-container ${activeSideBar && 'active'}`}>
-      <div  className="explore-title">
+      <div className="explore-title">
         <p>EXPLORER</p>
       </div>
-      <button type="button" 
-        className="explore-portfolio" 
-        onClick={() => setOpen(!open)}
-      >
+      <button type="button" className="explore-portfolio" onClick={toggleSidebar}>
         {open ? <IoIosArrowDown /> : <IoIosArrowForward />}
         {open ? <FaFolderOpen color="#EBCB8B"/> : <FaFolder color="#EBCB8B"/>}
         <p>Portfolio</p>
       </button>
       {open && (
         <>
-          <Link to="/" className="explorer-file">
-            <img src={ react_icon } alt="icon" className="icon-side-bar"/>
+          <Link to="/" className="explorer-file" onClick={toggleSidebar}>
+            <img src={react_icon} alt="icon" className="icon-side-bar"/>
             <p>home.jsx</p>
           </Link>
-          <Link to="/about" className="explorer-file">
+          <Link to="/about" onClick={toggleSidebar} className="explorer-file">
             <img src={ html_icon } alt="icon" className="icon-side-bar"/>
             <p>about.html</p>
           </Link>
-          <Link to="/projects" className="explorer-file">
+          <Link to="/projects" onClick={toggleSidebar} className="explorer-file">
             <img src={ js_icon } alt="icon" className="icon-side-bar"/>
             <p>project.js</p>
           </Link>
-          <Link to="/recommendation" className="explorer-file">
+          <Link to="/recommendation" onClick={toggleSidebar} className="explorer-file">
             <img src={ json_icon } alt="icon" className="icon-side-bar"/>
             <p>skills.json</p>
           </Link>
-          <Link to="/contact" className="explorer-file">
+          <Link to="/contact" onClick={toggleSidebar} className="explorer-file">
             <img src={ css_icon } alt="icon" className="icon-side-bar"/>
             <p>contact.css</p>
           </Link>
